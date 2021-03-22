63ae58a1436984737a7f5bfc0c0d025babedd4ad
Kyoko <kyokoconnect@gmail.com>
2021-03-22 19:12:35 -0400

    1st correction not passing all

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-function () {
-    
-    document.querySelector('html').innerHTML ('<body>')
-
+{
+    const body = document.querySelector('body');
+    body.innerHTML = ('<p>');
 }
-function () {
-    document.querySelector('html').innerHTML ('</body>')
 
-
+{
+    const body = document.querySelector('body');
+    body.innerHTML = ('<p>');
 }
-function () {
-    document.querySelector('body').innerHTML ('<h1>My HTML adventure</h1>')
+
+{
+    const body = document.querySelector('body');
+    body.innerHTML = ('</p>');
+}
+
+{
+    const p = document.querySelector('p');
+    p.innerHTML = ('<h1>My HTML adventure</h1>');
+}
+
+{
+    const p = document.querySelector('p');
+    p.innerHTML = ('<strong>');
+}
+
+{
+    const p = document.querySelector('p');
+    p.innerHTML = ('<em>');
+}
+
+{
+    const p = document.querySelector('p');
+    p.innerHTML = ('<a href="https://developer.mozilla.org/en-US/docs/Web/HTML">');
+}
+
+{
+    const body = document.querySelector('body');
+    body.innerHTML = ('<table>');
 }
